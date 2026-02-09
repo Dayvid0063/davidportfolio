534cab2e2b7d2c5693312cb350b15ba57fa670ed
Dayvid0063 <orjidavid63@gmail.com>
2026-02-09 14:49:40 +0100

    favi

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,6 +37,7 @@ export default defineNuxtConfig({
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
       ],
       link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap' },
         {
           rel: 'stylesheet',
